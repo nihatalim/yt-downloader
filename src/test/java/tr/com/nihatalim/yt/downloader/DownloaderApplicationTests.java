b79c275995d108394b3df1668d0d82b170379d35
Nihat Alim <iletisim@nihatalim.com.tr>
2022-02-27 16:07:26 +0300

    YT-1 Create Downloader application

---
+++ src/test/java/tr/com/nihatalim/yt/downloader/DownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.nihatalim.yt.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
